a9882167c0dd250fadec32779bc9c758c800d985
vicolat <vicolat1@gmail.com>
2026-05-19 15:59:07 +0100

    add get users route

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
-const userController = require("../controllers/userController");
-
-router.get("/users", userController.getUsers);
+router.get("/", async (req, res) => {
+  res.json({
+    message: "Users route working"
+  });
+});
 
 module.exports = router;
